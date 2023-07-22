d5a5c70d9256c8dc96740dadd28024dcc9b77098
sdkawata <sadaakikawata@gmail.com>
2023-07-23 02:57:04 +0900

    shim

--- src/main.js
+++ src/main.js
@@ -1,30 +1,79 @@
-import { init, WASI } from '@wasmer/wasi';
+import { WASI, File, OpenFile, PreopenDirectory } from "@bjorn3/browser_wasi_shim";
 
-(async () => {
-    await init();
-    let wasi = new WASI({
-        env: {
-            // 'ENVVAR1': '1',
-            // 'ENVVAR2': '2'
-        },
-        args: [
-            // 'command', 'arg1', 'arg2'
-        ],
-      });
-      
-      const module = await WebAssembly.compileStreaming(fetch('./toolkit.wasm'));
-      // const module = await WebAssembly.compileStreaming(fetch("https://deno.land/x/wasm/tests/demo.wasm"));
-      // Instantiate the WASI module
-      await wasi.instantiate(module, {
-        wasi_snapshot_preview1: {
-            sock_accept: function() {
-                throw "not supported";
-            }
+let module;
+
+const analyzeXhprof = async (name, buf) => {
+    let args = [
+        "tk",
+        // 'analyze-xhprof',
+        'generate-xhprof-graphviz',
+        name,
+    ];
+    let env = [];
+    const stdout = new File([])
+    const directory = new PreopenDirectory("/", {
+        [name]: new File(buf),
+    });
+    let fds = [
+        new OpenFile(new File([])), // stdin
+        new OpenFile(stdout), // stdout
+        new OpenFile(new File([])), // stderr
+        directory,
+    ];
+    let wasi = new WASI(args, env, fds);
+
+    const extendedImport = {
+        ...wasi.wasiImport,
+        sock_accept() {
+            throw "unimplemented"
         }
-      });
-      
-      // Run the start function
-      let exitCode = wasi.start();
-      let stdout = wasi.getStdoutString();
-      console.log(stdout)
-})()
+    }
+    const wrappedImport = {}
+    for (const key in extendedImport) {
+        // console.log(key)
+        wrappedImport[key] = function (...args) {
+            console.log("import called ", key, structuredClone(args))
+            const result = extendedImport[key].call(this, ...args)
+            console.log("result", result)
+            return result
+        }
+    }
+    // console.log(wrappedImport)
+    let inst = await WebAssembly.instantiate(module, {
+        "wasi_snapshot_preview1": {
+            ...wrappedImport,
+        }
+    });
+    try {
+        wasi.start(inst);
+    } catch(e) {
+        console.log(e)
+    }
+    // return new TextDecoder().decode(stdout.data)
+    return new TextDecoder().decode(directory.dir.contents["callgraph.dot"].data)
+}
+
+const init = async () => {
+    module = await WebAssembly.compileStreaming(fetch('./toolkit.wasm'));
+    const main = window.document.getElementById("main")
+    main.addEventListener('dragover', (e) => {e.preventDefault()})
+    main.addEventListener('drop', (e) => {
+        console.log('dropped')
+        e.preventDefault();
+        const item = e.dataTransfer.items[0]
+        if (item.kind !== "file") {
+            return
+        }
+        const file = item.getAsFile()
+        const reader = new FileReader()
+        const name = file.name
+        reader.onload = (e) => {
+            analyzeXhprof(name, e.target.result).then((result) => main.innerText = result)
+        }
+        reader.readAsArrayBuffer(file)
+    });
+    main.innerText = "initialized. waiting file droped..."
+}
+
+
+init()
